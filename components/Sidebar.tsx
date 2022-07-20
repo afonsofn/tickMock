72419d5ed98d6de74c13821480cc69e4ac2ae319
afonso <afonsoafn@gmail.com>
2022-07-20 14:42:06 -0300

    Construindo estilo dos butons do discover

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -27,18 +27,19 @@ const Sidebar = () => {
             </div>
             { showSideBar && (
                 <div className='xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3'>
-                    <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+                    <div className='xl:border-b-2 border-gray-200 xl:pb-4'> {/*  HOME BUTTON */}
                         <Link href='/'>
                             <div className={normalLink}>
-                                <p className="text-2x1">
+                                <p className="text-2xl">
                                     <AiFillHome />
                                 </p>
                                 <span className='text-lg hidden xl:block'>For You</span>
                             </div>
                         </Link>
                     </div>
+
                     { !userProfile && (
-                        <div className='px-2 py-4 hidden xl:block'>
+                        <div className='px-2 py-4 hidden xl:block'> {/*  LOGIN BUTTON ON LARGE SCREEN */}
                             <p className='text-gray-400'>
                                 Log in to like and comment on videos
                             </p>
